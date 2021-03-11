0745999b88ad1544c4e6ec6b17acff82539b1ebd
damian Howard <daho2353@colorado.edu>
2021-03-10 22:49:03 -0700

    added Education Skeleton

--- my-app/src/Resume.js
+++ my-app/src/Resume.js
@@ -47,6 +47,15 @@ function Resume(props)
             <WorkTile desc= "test" task1="task1" task2="task2" task3="task3" task4="task4"/>
             <WorkTile desc= "test" task1="task1" task2="task2" task3="task3" task4="task4"/>
             <WorkTile desc= "test" task1="task1" task2="task2" task3="task3" task4="task4"/>
+            <h2 className="underlined"> Education </h2>
+            <div className="CaptionedImage">
+                <img width="150px" height="150px"/>
+                <p classname = "caption"> Univeristy of Colorado Boulder <br/> Major : Technology,Arts and Media <br/> Minor: Computer Science <br/> GPA: 3.1 </p>
+            </div>
+            <div className="CaptionedImage">
+                <img width="150px" height="150px"/>
+                <p classname = "caption"> Centaurus High School  <br/> International Baccalaureate Degree <br/> Pre-Engineering honors <br/>  Graduated top 20 % of my class </p>
+            </div>
         </section>
        
     )

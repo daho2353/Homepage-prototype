ed3819ea3caeb7dacb8b9c334a7e6dac982f2880
damian Howard <daho2353@colorado.edu>
2021-03-14 00:24:29 -0700

    added ReactPlayer library to embed video, started linking out to projects and tweaked css

--- my-app/src/Portfolio.js
+++ my-app/src/Portfolio.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import Project from './Project.js';
+import ReactPlayer from "react-player";
 
+const videoStyle = {
+    display: 'inline-block',
+    width: '300px',
+};
 function Portfolio(props)
 {
     if (props.activeCheck === "Portfolio") return(
         <section className = "Portfolio-container">
             <h2> Software Development Projects</h2>
-            <Project title="Discord Bot" size="large" website="_blank" image="title.jpg" projectText="words" />
-            <Project title="A Space Bear Adventure" size="large" website="_blank" image="title.jpg" projectText="words" />
+            <Project title="Discord Bot" size="large" website="https://github.com/daho2353/Discord-Bot" image="title.jpg" projectText="words" />
+            <div className="project-container">
+                <h1> A Space Bear Adventure</h1>
+                <ReactPlayer url="https://youtu.be/ie4Q3VwbWSA" style={videoStyle} width="500px" />
+                <p> Project Description </p>
+            </div>
             <Project title="Arduino Projects" size="large" website="_blank" image="title.jpg" projectText="words" />
             <Project title="Connect 4" size="large" website="_blank" image="title.jpg" projectText="words" />
             <Project title="Discover M(you)sic (senior captsone)" size="large" website="_blank" image="title.jpg" projectText="words" />
--- my-app/src/Project.js
+++ my-app/src/Project.js
@@ -1,19 +1,20 @@
 import React from 'react';
 
+
 function Project(props)
 {
     
     if (props.size === "large") return (
         <section className ="project-container">
             <h1> {props.title} </h1>
-            <a href={props.website}><img src={props.image} height ="150px" width="150px"/></a>
+            <a href={props.website}><img src={props.image} height ="300px" width="500px"/></a>
             <p className="project-description"> {props.projectText}</p>
         </section>
     )
     else return (
         <section className ="project-container">
             <h1> {props.title} </h1>
-            <a href={props.website}><img src={props.image} height ="50px" width="50px"/></a>
+            <a href={props.website}><img src={props.image} height ="100px" width="100px"/></a>
             <p className="project-description"> {props.projectText}</p>
         </section>
 

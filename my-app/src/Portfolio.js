fb4cb927044e7fc224fbce9746fbf83f3c7cccce
damian Howard <daho2353@colorado.edu>
2021-03-13 23:51:45 -0700

    structured Portfolio

--- my-app/src/Portfolio.js
+++ my-app/src/Portfolio.js
@@ -6,12 +6,33 @@ function Portfolio(props)
     if (props.activeCheck === "Portfolio") return(
         <section className = "Portfolio-container">
             <h2> Software Development Projects</h2>
-            <Project title="hey" website="_blank" image="title.jpg" projectText="words"/>
+            <Project title="Discord Bot" size="large" website="_blank" image="title.jpg" projectText="words" />
+            <Project title="A Space Bear Adventure" size="large" website="_blank" image="title.jpg" projectText="words" />
+            <Project title="Arduino Projects" size="large" website="_blank" image="title.jpg" projectText="words" />
+            <Project title="Connect 4" size="large" website="_blank" image="title.jpg" projectText="words" />
+            <Project title="Discover M(you)sic (senior captsone)" size="large" website="_blank" image="title.jpg" projectText="words" />
+            <Project title="Pico 8 - Ice Puzzles" size="large" website="_blank" image="title.jpg" projectText="words" />
+            <Project title="Dark Soul Fan Wiki" size="large" website="_blank" image="title.jpg" projectText="words" />
+            <Project title="Matching Game" size="large" website="_blank" image="title.jpg" projectText="words" />
+            <Project title="Javascript Clock" size="large" website="_blank" image="title.jpg" projectText="words" />
+            <Project title="My previous home page" size="large" website="_blank" image="title.jpg" projectText="words" />
+            <br/>
+            <Project title="Github Group Practice" size="small" website="_blank" image="title.jpg" projectText="words" />
+            <Project title="Bash Terminal Practice" size="small" website="_blank" image="title.jpg" projectText="words" />
+            <Project title="Parralax Gaming History" size="small" website="_blank" image="title.jpg" projectText="words" />
+            <Project title="Event Listener Practice" size="small" website="_blank" image="title.jpg" projectText="words" />
+            <Project title="Firebase and Forms Practice" size="small" website="_blank" image="title.jpg" projectText="words" />
+            <Project title="HTML Forms Practice" size="small" website="_blank" image="title.jpg" projectText="words" />
+            <Project title="Dom Manipulation" size="small" website="_blank" image="title.jpg" projectText="words" />
+            <Project title="React To-Do List" size="small" website="_blank" image="title.jpg" projectText="words" />
+            <Project title= "Flexbox Practice" size="small" website="_blank" image="title.jpg" projectText="words" />
+            <Project title= "Pico 8 - Tiny Games" size="small" website="_blank" image="title.jpg" projectText="words" />
+            <Project title="Bitsy - Tiny Games" size="small" website="_blank" image="title.jpg" projectText="words" />
+            <Project title="Unity SHMUP" size="small" website="_blank" image="title.jpg" projectText="words" />
         </section>
     )
     else return(
         <p></p>
       )
 }
-
 export default Portfolio;
--- my-app/src/Project.js
+++ my-app/src/Project.js
@@ -2,13 +2,23 @@ import React from 'react';
 
 function Project(props)
 {
-    return(
-        <section className ="Project-Container">
+    
+    if (props.size === "large") return (
+        <section className ="project-container">
             <h1> {props.title} </h1>
-            <a href={props.website}><img src={props.image} height ="50px" width="50px"/></a>
-            <p> {props.projectText}</p>
+            <a href={props.website}><img src={props.image} height ="150px" width="150px"/></a>
+            <p className="project-description"> {props.projectText}</p>
         </section>
     )
-}
+    else return (
+        <section className ="project-container">
+            <h1> {props.title} </h1>
+            <a href={props.website}><img src={props.image} height ="50px" width="50px"/></a>
+            <p className="project-description"> {props.projectText}</p>
+        </section>
+
+    ) 
+
+} 
 
 export default Project;

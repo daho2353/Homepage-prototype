64a1487681ae2f9bd4040b3e8f2ec01fb761d369
damian Howard <daho2353@colorado.edu>
2021-03-24 23:00:50 -0600

    removed button from worktile, played with font weight in App.css

--- my-app/src/WorkTile.js
+++ my-app/src/WorkTile.js
@@ -5,8 +5,7 @@ function WorkTile(props){
     return(
         <section className ="work-tile">
                 <img src={props.src} width="150px" height="150px"/>
-                <button className="work-tile-button"> click me for info about this job </button>
-                <p className="job-description">{props.desc} test</p>
+                <p className="job-description">{props.desc}</p>
                 <h3> Work Tasks </h3>
                 <p className="job-description"> - {props.task1} </p>
                 <p className="job-description"> - {props.task2} </p>
